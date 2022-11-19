ef9bbe5b9b067aee27b1f4f69811064d6bf4eed1
natlaw <nat2llaw@gmail.com>
2022-11-19 15:39:01 +0000

    get all items

--- app.js
+++ app.js
@@ -8,19 +8,23 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const sharp = require ("sharp")
 const fs = require("fs");
-const {
-  postContainer,
-  updateContainerById,
-} = require("./models/containers-model");
-
 
 const { getAllUsers, addUser } = require("./controllers/user-controller")
+
 const {
   getAllContainers,
   addContainer,
   getContainerById,
   getRooms,
 } = require("./controllers/containers-controller");
+
+const {
+  postContainer,
+  updateContainerById,
+} = require("./models/containers-model");
+
+const {getAllItems} = require("./controllers/items-container")
+
 const {getImageById} = require("./controllers/images-controller")
 
 const {
@@ -41,7 +45,7 @@ app.post("/api/add_user", addUser);
 
 app.get("/api/containers", getAllContainers);
 
-app.post("/api/add_container", addContainer);
+app.post("/api/add_container", addContainer); //don't use this one to add new containers
 
 app.get("/api/images/:id", getImageById);
 
@@ -112,58 +116,14 @@ const addNewContainer = (req, res, next) => {
   });
 };
 
-app.post("/api/containers/addcontainer/:parent_id", upload.single("file"), addNewContainer);
+app.post("/api/containers/addcontainer/:parent_id", upload.single("file"), addNewContainer); //use this one to add a new container
 
 app.get("/api/rooms", getRooms)
 
+app.get("/api/items", getAllItems)
+
 // getAllItems, addItem, getItemById
 
-//post image, get image ref by id, created container with image ref and parent, find one and update parent
-
-/*app.post("/api/image").then((imageID)>{
-
-  parentId
-  imageID
-
-  post("/api/container").then((containerid)=>{
-
-    findoneandUp(parentID,  $push    contains:[containerID]).then(()=>{
-
-
-      res.send("container created:", container)
-    })
-  })
-
-
-exporrts.addContainer = (req, res, next) => {
-  postImage(image).then((image)=> {
-    postContainer(image.id).then((container)=>{
-      
-      
-    nestElement(containerid).then((container)=>{
-
-      res.send(container)
-    
-    })
-    })
-  
-  })
-}
-
-app.post("/api/containers/addcontainer/:parent_id")
-
-insominia
-
-file  (pick a file)
-
-name: container-name
-description:
-
-
-parent_id
-contains
-
-}) */
 
 app.post("/api/add_item", async (request, response) => {
   const item = new itemModel(request.body);
